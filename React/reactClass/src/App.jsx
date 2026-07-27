f716e58d6f26c712df09cf85a9cd69bd0e015161
Abdul Rehman <abdulrehmanpro6@gmail.com>
2026-07-27 14:34:44 +0500

    install react

---
+++ React/reactClass/src/App.jsx
@@ -0,0 +1,17 @@
+import Navbar from "./components/Navbar"
+import Home from "./pages/Home"
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <Navbar/>
+        <Home/>
+        <h1>hello world</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ React/reactClass/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+const Home =() =>{
+
+
+    return(
+
+
+        <div  className="w-full h-screen bg-amber-900 p-4  flex justify-between items-center">
+            Navbar
+
+
+        </div>
+    )
+
+}
+export default Home
